d3d282fab9c997f5132de056a7828d3f48c412d7
julio <juliolapon2002@gmail.com>
2025-04-07 12:33:58 -0400

    Initial commit of model

---
+++ main.py
@@ -0,0 +1,26 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are in an expert in answering Questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is a question: {question}"""
+
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    print("\n\n--------------------------------------")
+    question = input("Enter a question(q to quit): ")
+    print("\n\n")
+    if question == "q":
+        break
+    
+    reviews =retriever.invoke(question)
+    result = chain.invoke({"reviews":reviews, "question": question})
+    print(result)  
